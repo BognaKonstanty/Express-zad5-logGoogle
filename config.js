6825d981b9d4f36b782c14617084c945217889bc
Bogna Konstanty <rosiakb@gmail.com>
2017-07-27 17:07:31 +0200

    add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '963829640238-5cetakhii81p6bqvvsbc3t2a5483hlop.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'EZ8MJyus4pFLr_G5GoWtd0um',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
